a4bec5ba32ae8548e16ebc95b27d4415b76b39a0
JOONHYUK LEE <joonhyuk@JOONHYUKs-MacBook-Air.local>
2025-01-05 13:28:59 +0900

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Strting up...");
+  console.log("Strting up.....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
